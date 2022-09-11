413fd6f363c13663e535520867dac6733d6b7f50
mohammadaminsaheb <mohammadaminsaheb@gmail.com>
2022-09-11 08:26:53 -0700

    creating an static schema from table component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
+import Table from "./components/Table";
+
 function App() {
   return (
     <div className="w-full md:flex items-start justify-between px-8 gap-4">
       <main className="w-full md:w-4/6 bg-slate-600">
-dasdd
+        <Table/>
       </main>
       <aside className="w-full md:w-2/6 bg-red-600">
 sdada
--- src/components/Table.js
+++ src/components/Table.js
@@ -1,7 +1,20 @@
 const Table = () => {
     return (
         <div>
-
+            <table>
+                <tr>
+                    <th>title 1</th>
+                    <th>title 1</th>
+                    <th>title 1</th>
+                    <th>title 1</th>
+                </tr>
+                <tr>
+                    <td>sdada</td>
+                    <td>sdada</td>
+                    <td>sdada</td>
+                    <td>sdada</td>
+                </tr>
+            </table>
         </div>
     );
 }
